d01adf3f9d2021a3cc6a499700b379f428e3bfac
Evandro de Oliveira <evandro.oliveira@univille.br>
2025-05-21 12:00:01 -0300

    Change User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,20 +2,19 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Model
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $fillable = [
         'name',
@@ -26,23 +25,46 @@ class User extends Authenticatable
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $hidden = [
         'password',
+        'email_verified_at',
         'remember_token',
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * The attributes that should be cast.
      *
-     * @return array<string, string>
+     * @var array<string, string>
      */
-    protected function casts(): array
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    /**
+     * Get user by email
+     * 
+     * @param string $pEmail - The user email
+     * @param bool $makeVisible - Defines whether the password should be visible.
+     * @return array|self
+     */
+    public static function userByEmail(string $pEmail, bool $pMakeVisible = false): array|self
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        $fetchRow = self::query()
+            ->filters(['email' => $pEmail])
+            ->first();
+
+        if (!$fetchRow) {
+            return [];
+        }
+
+        if ($pMakeVisible) {
+            $fetchRow->makeVisible(['password']);
+        }
+
+
+        return $fetchRow;
     }
 }

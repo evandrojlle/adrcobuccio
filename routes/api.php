7876d286d92f413a0b4fd98dbb2f55d0d5ea08bc
Evandro de Oliveira <evandro.oliveira@univille.br>
2025-05-21 15:47:13 -0300

    Fixed route by id

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,6 @@ Route::post('auth', [AuthController::class, 'auth']);
 Route::post('/user/store', [UsersController::class, 'store']);
 Route::prefix('user')->group(function () {
     Route::put('update', [UsersController::class, 'update']);
-    Route::get('id/{id}', [UsersController::class, 'get']);
+    Route::get('{id}', [UsersController::class, 'get'])->where('id', '[0-9]+');
     Route::get('filters/{value?}', [UsersController::class, 'filters'])->where('value', '.*');
 });

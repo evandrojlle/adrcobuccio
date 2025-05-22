31270a27d1d6851749acb449c5ea9833374714fb
Evandro de Oliveira <evandro.oliveira@univille.br>
2025-05-22 10:47:53 -0300

    Added php doc

--- app/Http/Controllers/WalletsController.php
+++ app/Http/Controllers/WalletsController.php
@@ -14,7 +14,7 @@ class WalletsController extends Controller
     use Log;
 
     /**
-     * Self deposite money to wallet
+     * Self Credit to wallet
      *
      * @param WalletRequest $request - Request form data
      * @return JsonResponse
@@ -22,9 +22,9 @@ class WalletsController extends Controller
     public function selfCredit(WalletRequest $request): JsonResponse
     {
         try {
-            $validated = $request->validated();
-            $wallet = $this->walletTransaction($validated);
-            if (! $wallet->id) {
+            $validated = $request->validated(); // Validate request data.
+            $wallet = $this->walletTransaction($validated); // Save transaction into wallet.
+            if (! $wallet->id) { // if failed to save wallet transaction, return error response.
                 return response()->json([
                     'success' => false,
                     'message' => __('An error occurred while deposite money to wallet.'),
@@ -32,11 +32,12 @@ class WalletsController extends Controller
                 ], 400);
             }
 
+            // Transform wallet data to array and add transaction type and amount transaction.
             $walletTransaction = $wallet->toArray();
             $walletTransaction['type_transaction'] = 'CREDIT';
             $walletTransaction['amount_transaction'] = $validated['amount_transaction'];
-            $bankStatement = $this->bankStatement($walletTransaction);
-            if (! $bankStatement->id) {
+            $bankStatement = $this->bankStatement($walletTransaction); // Save statement into bank statement.
+            if (! $bankStatement->id) { // if failed to save bank statement, return error response.
                 return response()->json([
                     'success' => false,
                     'message' => __('An error occurred while saving the transaction data.'),
@@ -44,6 +45,7 @@ class WalletsController extends Controller
                 ], 400);
             }
 
+            // return success response with wallet data.
             return response()->json([
                 'success' => true,
                 'message' => __('Money deposite successfully.'),
@@ -54,7 +56,7 @@ class WalletsController extends Controller
                 ]
             ], 200);
         } catch (\Exception $e) {
-            Log::save('error', $e);
+            Log::save('error', $e); // Save error log.
 
             return response()->json([
                 'success' => false,
@@ -64,11 +66,18 @@ class WalletsController extends Controller
         }
     }
 
-    private function walletTransaction(array $data): Wallet
+    /**
+     * Save transaction into wallet.
+     *
+     * @param array $wallet - Wallet data transaction.
+     * @return Wallet
+     */
+    private function walletTransaction(array $wallet): Wallet
     {
-        $wallet = Wallet::firstByFilters(['owner_id' => $data['owner_id']]);
+        // Check if wallet exists. If exists, update the amount and update_at, else create a new data wallet.
+        $wallet = Wallet::firstByFilters(['owner_id' => $wallet['owner_id']]);
         if ($wallet) {
-            $amount = $wallet->amount + $data['amount_transaction'];
+            $amount = $wallet->amount + $wallet['amount_transaction'];
             $wallet->amount = $amount;
             $wallet->updated_at = Carbon::now()->format('Y-m-d H:i:s');
             $wallet->save();
@@ -77,7 +86,7 @@ class WalletsController extends Controller
             $wallet->created_at = Carbon::now()->format('Y-m-d H:i:s');
             $wallet->updated_at = null;
             
-            foreach ($data as $key => $value) {
+            foreach ($wallet as $key => $value) {
                 $key = ($key == 'amount_transaction') ? 'amount' : $key;
                 $wallet->$key = $value;
             }
@@ -88,8 +97,15 @@ class WalletsController extends Controller
         return $wallet;
     }
 
+    /**
+     * Save transaction into bank statement.
+     *
+     * @param array $wallet - Wallet data transaction.
+     * @return BankStatement
+     */
     private function bankStatement(array $wallet): BankStatement
     {
+        // Save statement into bank statement.
         $bankStatement = new BankStatement();
         $bankStatement->wallet_id = $wallet['id'];
         $bankStatement->transfer_id = $wallet['owner_id'];
